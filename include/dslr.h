d970f0fe51ed740a300e774154b1fc1e349a5552
Howard Lau <howardlau1999@hotmail.com>
2023-09-01 06:48:47 +0000

    use unsafe

--- include/dslr.h
+++ include/dslr.h
@@ -17,17 +17,27 @@ using namespace std::chrono_literals;
 
 auto const kLeaseTime = 10ms;
 auto const kWaitTime = 5us;
+auto const kRandomBackoffBase = 10ULL;
+auto const kRandomBackoffLimit = 10000ULL;
 
-inline auto randomBackoffDuration() {
+auto const kExclusiveCounterBitOffset = 48;
+auto const kSharedCounterBitOffset = 32;
+auto const kExclusiveMaxBitOffset = 16;
+auto const kSharedMaxBitOffset = 0;
+
+inline auto random_backoff_duration(int nr_conflicts) {
   static std::random_device rd;
   static std::mt19937 gen(rd());
-  static std::uniform_int_distribution<> dis(0, 10);
-  return std::chrono::milliseconds(dis(gen));
+  auto const lower = 0ULL;
+  auto const upper = std::min(kRandomBackoffLimit, kRandomBackoffBase * (1ULL << std::min(32, nr_conflicts)));
+  std::uniform_int_distribution<> dis(lower, upper);
+  
+  return std::chrono::microseconds(dis(gen));
 }
 
-inline void random_backoff() {
+inline void random_backoff(int nr_conflicts) {
   auto start = std::chrono::steady_clock::now();
-  auto delay = randomBackoffDuration();
+  auto delay = random_backoff_duration(nr_conflicts);
   while (std::chrono::steady_clock::now() - start < delay) {
     asm volatile("pause" ::: "memory");
   }
@@ -39,34 +49,34 @@ struct lock_state {
   explicit lock_state(uint64_t state) : state_(state) {}
   lock_state(uint16_t exclusive_counter, uint16_t shared_counter,
              uint16_t exclusive_max, uint16_t shared_max)
-      : state_((static_cast<uint64_t>(exclusive_counter) << 48) |
-               (static_cast<uint64_t>(shared_counter) << 32) |
-               (static_cast<uint64_t>(exclusive_max) << 16) |
+      : state_((static_cast<uint64_t>(exclusive_counter) << kExclusiveCounterBitOffset) |
+               (static_cast<uint64_t>(shared_counter) << kSharedCounterBitOffset) |
+               (static_cast<uint64_t>(exclusive_max) << kExclusiveMaxBitOffset) |
                static_cast<uint64_t>(shared_max)) {}
   lock_state(const lock_state &) = default;
   lock_state &operator=(const lock_state &) = default;
   lock_state(lock_state &&) = default;
   lock_state &operator=(lock_state &&) = default;
-  uint16_t exclusive_counter() const { return (state_ >> 48) & 0xffff; }
-  uint16_t shared_counter() const { return (state_ >> 32) & 0xffff; }
-  uint16_t exclusive_max() const { return (state_ >> 16) & 0xffff; }
+  uint16_t exclusive_counter() const { return (state_ >> kExclusiveCounterBitOffset) & 0xffff; }
+  uint16_t shared_counter() const { return (state_ >> kSharedCounterBitOffset) & 0xffff; }
+  uint16_t exclusive_max() const { return (state_ >> kExclusiveMaxBitOffset) & 0xffff; }
   uint16_t shared_max() const { return state_ & 0xffff; }
 };
 
 class shared_mutex {
 private:
   rdmapp::remote_mr remote_mr_;
-  std::shared_ptr<rdmapp::local_mr> prev_state_mr_;
-  std::shared_ptr<rdmapp::local_mr> curr_state_mr_;
-  std::shared_ptr<rdmapp::qp> qp_;
+  rdmapp::local_mr* prev_state_mr_;
+  rdmapp::local_mr* curr_state_mr_;
+  rdmapp::qp* qp_;
   static constexpr uint16_t kCountMax = 32768;
   std::unordered_map<uint64_t, lock_state> reset_from_;
 
 public:
   shared_mutex(rdmapp::remote_mr remote_mr,
-               std::shared_ptr<rdmapp::local_mr> prev_state_mr,
-               std::shared_ptr<rdmapp::local_mr> curr_state_mr,
-               std::shared_ptr<rdmapp::qp> qp)
+               rdmapp::local_mr* prev_state_mr,
+               rdmapp::local_mr* curr_state_mr,
+               rdmapp::qp* qp)
       : remote_mr_(remote_mr), prev_state_mr_(prev_state_mr),
         curr_state_mr_(curr_state_mr), qp_(qp) {}
 
--- src/dslr.cc
+++ src/dslr.cc
@@ -1,14 +1,21 @@
 #include "dslr.h"
 
 namespace dslr {
-  rdmapp::task<bool> shared_mutex::lock_shared(uint64_t txn_id) {
+rdmapp::task<bool> shared_mutex::lock_shared(uint64_t txn_id) {
+  int nr_conflicts = 0;
+  while (true) {
     co_await qp_->fetch_and_add(remote_mr_, prev_state_mr_, 1);
     auto prev = *reinterpret_cast<lock_state *>(prev_state_mr_->addr());
     if (prev.shared_max() >= kCountMax || prev.exclusive_max() >= kCountMax) {
       co_await qp_->fetch_and_add(remote_mr_, prev_state_mr_, -1);
       // Backoff
-      random_backoff();
-      // HandleConflict
+      {
+        random_backoff(nr_conflicts);
+        ++nr_conflicts;
+        continue;
+      }
+
+      // prev ns or prev nx has not changed for longer than twice the lease time
 
       co_return false;
     } else if (prev.shared_max() == kCountMax - 1) {
@@ -22,18 +29,24 @@ namespace dslr {
       co_return false;
     }
   }
+}
 
-  rdmapp::task<bool> shared_mutex::lock(uint64_t txn_id) {
+rdmapp::task<bool> shared_mutex::lock(uint64_t txn_id) {
+  int nr_conflicts = 0;
+  while (true) {
     co_await qp_->fetch_and_add(remote_mr_, prev_state_mr_,
-                                1 << (sizeof(uint16_t) * 8));
+                                1 << kExclusiveMaxBitOffset);
     auto prev = *reinterpret_cast<lock_state *>(prev_state_mr_->addr());
     if (prev.exclusive_max() >= kCountMax || prev.shared_max() >= kCountMax) {
       co_await qp_->fetch_and_add(remote_mr_, prev_state_mr_,
-                                  0xFFFFULL << (8 * sizeof(uint16_t)));
-      // Backoff
-      random_backoff();
+                                  0xFFFFULL << kExclusiveMaxBitOffset);
+      {
+        random_backoff(nr_conflicts);
+        ++nr_conflicts;
+        continue;
+      }
 
-      // HandleConflict
+      // prev ns or prev nx has not changed for longer than twice the lease time
 
       co_return false;
     } else if (prev.exclusive_max() == kCountMax - 1) {
@@ -48,76 +61,78 @@ namespace dslr {
       co_return false;
     }
   }
+}
 
-  rdmapp::task<bool> shared_mutex::unlock_shared(uint64_t txn_id) {
-    co_await qp_->fetch_and_add(remote_mr_, curr_state_mr_,
-                                1ULL << (2 * sizeof(uint16_t) * 8));
+rdmapp::task<bool> shared_mutex::unlock_shared(uint64_t txn_id) {
+  co_await qp_->fetch_and_add(remote_mr_, curr_state_mr_,
+                              1ULL << kSharedCounterBitOffset);
 
-    co_return true;
-  }
+  co_return true;
+}
 
-  rdmapp::task<bool> shared_mutex::unlock(uint64_t txn_id) {
-    co_await qp_->fetch_and_add(remote_mr_, curr_state_mr_,
-                                1ULL << (3 * sizeof(uint16_t) * 8));
+rdmapp::task<bool> shared_mutex::unlock(uint64_t txn_id) {
+  co_await qp_->fetch_and_add(remote_mr_, curr_state_mr_,
+                              1ULL << kExclusiveCounterBitOffset);
 
-    co_return true;
-  }
+  co_return true;
+}
 
-  rdmapp::task<void> shared_mutex::reset_lock(uint64_t txn_id) {
-    while (true) {
-      co_await qp_->compare_and_swap(remote_mr_, curr_state_mr_,
-                                     reset_from_[txn_id].state_, 0);
-      auto curr = *reinterpret_cast<lock_state *>(curr_state_mr_->addr());
-      if (curr.state_ == 0) {
-        reset_from_[txn_id] = lock_state(0);
-        co_return;
-      }
-    }
-  }
-
-  rdmapp::task<bool> shared_mutex::handle_conflict(uint64_t txn_id, lock_state prev,
-                                     bool exclusive) {
-    while (true) {
-      co_await qp_->read(remote_mr_, curr_state_mr_);
-      auto curr = *reinterpret_cast<lock_state *>(curr_state_mr_->addr());
-      if (!exclusive) {
-        if (prev.exclusive_max() == curr.exclusive_counter()) {
-          co_return true;
-        }
-      } else {
-        if (prev.exclusive_max() == curr.exclusive_counter() &&
-            prev.shared_max() == curr.shared_counter()) {
-          co_return true;
-        }
-      }
-      // TODO
-      if (true) {
-        lock_state reset_state;
-        if (!exclusive) {
-          reset_state = lock_state(prev.exclusive_max(), prev.shared_max() + 1,
-                                   curr.exclusive_max(), curr.shared_max());
-        } else {
-          reset_state = lock_state(prev.exclusive_max() + 1, prev.shared_max(),
-                                   curr.exclusive_max(), curr.shared_max());
-        }
-        co_await qp_->compare_and_swap(remote_mr_, curr_state_mr_, prev.state_,
-                                       reset_state.state_);
-        auto cas_curr = *reinterpret_cast<lock_state *>(curr_state_mr_->addr());
-        if (cas_curr.state_ == reset_state.state_) {
-          if (reset_state.exclusive_max() >= kCountMax ||
-              reset_state.shared_max() >= kCountMax) {
-            // TODO: Reset to 0
-          }
-          co_return false;
-        }
-      }
-      auto sum = prev.exclusive_max() - curr.exclusive_counter() +
-                 prev.shared_max() - curr.shared_counter();
-      auto start = std::chrono::steady_clock::now();
-      auto wait_delay = sum * kWaitTime;
-      while (std::chrono::steady_clock::now() - start < wait_delay) {
-        asm volatile("pause" ::: "memory");
-      }
+rdmapp::task<void> shared_mutex::reset_lock(uint64_t txn_id) {
+  while (true) {
+    co_await qp_->compare_and_swap(remote_mr_, curr_state_mr_,
+                                   reset_from_[txn_id].state_, 0);
+    auto curr = *reinterpret_cast<lock_state *>(curr_state_mr_->addr());
+    if (curr.state_ == 0) {
+      reset_from_[txn_id] = lock_state(0);
+      co_return;
     }
   }
 }
+
+rdmapp::task<bool> shared_mutex::handle_conflict(uint64_t txn_id,
+                                                 lock_state prev,
+                                                 bool exclusive) {
+  while (true) {
+    co_await qp_->read(remote_mr_, curr_state_mr_);
+    auto curr = *reinterpret_cast<lock_state *>(curr_state_mr_->addr());
+    if (!exclusive) {
+      if (prev.exclusive_max() == curr.exclusive_counter()) {
+        co_return true;
+      }
+    } else {
+      if (prev.exclusive_max() == curr.exclusive_counter() &&
+          prev.shared_max() == curr.shared_counter()) {
+        co_return true;
+      }
+    }
+    // TODO
+    if (true) {
+      lock_state reset_state;
+      if (!exclusive) {
+        reset_state = lock_state(prev.exclusive_max(), prev.shared_max() + 1,
+                                 curr.exclusive_max(), curr.shared_max());
+      } else {
+        reset_state = lock_state(prev.exclusive_max() + 1, prev.shared_max(),
+                                 curr.exclusive_max(), curr.shared_max());
+      }
+      co_await qp_->compare_and_swap(remote_mr_, curr_state_mr_, prev.state_,
+                                     reset_state.state_);
+      auto cas_curr = *reinterpret_cast<lock_state *>(curr_state_mr_->addr());
+      if (cas_curr.state_ == reset_state.state_) {
+        if (reset_state.exclusive_max() >= kCountMax ||
+            reset_state.shared_max() >= kCountMax) {
+          // TODO: Reset to 0
+        }
+        co_return false;
+      }
+    }
+    auto sum = prev.exclusive_max() - curr.exclusive_counter() +
+               prev.shared_max() - curr.shared_counter();
+    auto start = std::chrono::steady_clock::now();
+    auto wait_delay = sum * kWaitTime;
+    while (std::chrono::steady_clock::now() - start < wait_delay) {
+      asm volatile("pause" ::: "memory");
+    }
+  }
+}
+} // namespace dslr
